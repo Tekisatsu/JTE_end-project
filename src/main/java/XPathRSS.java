10b7dd8e97674ddfdf282c706cd1dd226c178d2a
Tekisatsu <ale.hartikainen@gmail.com>
2026-02-18 16:07:53 +0200

    empty and non 2.0 tests and code done

---
+++ src/main/java/XPathRSS.java
@@ -0,0 +1,69 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import java.net.URI;
+
+public class XPathRSS {
+    public static void main(String[] args) throws Exception {
+        try {
+            if (args.length != 1) {
+                throw new Exception("No arguments");
+            }
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            Document doc = factory.newDocumentBuilder().parse(URI.create((args[0])).toURL().openStream());
+
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+
+            String version = xpath.evaluate("/rss/@version", doc);
+            if (!version.equals("2.0")) {
+                throw new Exception("Not RSS 2.0");
+            }
+            XPathExpression exprChannel = xpath.compile("//channel");
+            NodeList itemsChannel = (NodeList) exprChannel.evaluate(doc, XPathConstants.NODESET);
+            if (itemsChannel.getLength() == 0) {
+                throw new Exception("Empty channel");
+            }
+            for (int i = 0; i < itemsChannel.getLength(); i++) {
+                Node item = itemsChannel.item(i);
+                String title = xpath.evaluate("title", item);
+                String description = xpath.evaluate("description", item);
+                String generator = xpath.evaluate("generator", item);
+                String link = xpath.evaluate("link", item);
+
+                System.out.println("Channel Title: " + title);
+                System.out.println("Channel Description: " + description);
+                System.out.println("Channel Generator: " + generator);
+                System.out.println("Channel Link: " + link);
+                System.out.println("-----------------------");
+
+            }
+
+            XPathExpression expr = xpath.compile("//item");
+            NodeList items = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+            if (items.getLength() == 0) {
+                throw new Exception("No item");
+            }
+
+            for (int i = 0; i < items.getLength(); i++) {
+                Node item = items.item(i);
+                String title = xpath.evaluate("title", item);
+                String link = xpath.evaluate("link", item);
+                String pubDate = xpath.evaluate("pubDate", item);
+                System.out.println("Title: " + title);
+                System.out.println("Link: " + link);
+                System.out.println("Date: " + pubDate);
+                System.out.println("Description:");
+                XPathExpression exprItemDesc = xpath.compile("./description/text()");
+                NodeList itemsDesc = (NodeList) exprItemDesc.evaluate(doc, XPathConstants.NODESET);
+                for (i = 0; i < itemsDesc.getLength(); i++) {
+                    System.out.println(itemsDesc.item(i));
+                }
+                System.out.println("-----------------------");
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/XPathRSSTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XPathRSSTest {
+
+    @Test
+    void testMain() {
+        String notRSSuri = "https://www.w3schools.com/xml/note.xml";
+        Throwable empty = assertThrows(Exception.class, () -> {XPathRSS.main(new String[]{});});
+        assertEquals("No arguments", empty.getMessage());
+        Throwable notRss = assertThrows(Exception.class, () -> {XPathRSS.main(new String[] {notRSSuri});});
+        assertEquals("Not RSS 2.0", notRss.getMessage());
+    }
+}
