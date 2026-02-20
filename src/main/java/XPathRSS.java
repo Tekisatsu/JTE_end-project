08ad3c3bd8774d4d096572537d24712650756c02
Tekisatsu <ale.hartikainen@gmail.com>
2026-02-20 17:50:35 +0200

    desc and other data fetching done

--- src/main/java/XPathRSS.java
+++ src/main/java/XPathRSS.java
@@ -36,11 +36,12 @@ public class XPathRSS {
                 System.out.println("Channel Description: " + description);
                 System.out.println("Channel Generator: " + generator);
                 System.out.println("Channel Link: " + link);
-                System.out.println("-----------------------");
+                System.out.println("++++++++++++++++++++");
 
             }
 
             XPathExpression expr = xpath.compile("//item");
+            XPathExpression descExpr = xpath.compile(".//description"); // Note the './/' for relative search
             NodeList items = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
             if (items.getLength() == 0) {
                 throw new Exception("No item");
@@ -49,18 +50,17 @@ public class XPathRSS {
             for (int i = 0; i < items.getLength(); i++) {
                 Node item = items.item(i);
                 String title = xpath.evaluate("title", item);
+                NodeList descriptions = (NodeList) descExpr.evaluate(item, XPathConstants.NODESET);
                 String link = xpath.evaluate("link", item);
                 String pubDate = xpath.evaluate("pubDate", item);
                 System.out.println("Title: " + title);
                 System.out.println("Link: " + link);
                 System.out.println("Date: " + pubDate);
-                System.out.println("Description:");
-                XPathExpression exprItemDesc = xpath.compile("./description/text()");
-                NodeList itemsDesc = (NodeList) exprItemDesc.evaluate(doc, XPathConstants.NODESET);
-                for (i = 0; i < itemsDesc.getLength(); i++) {
-                    System.out.println(itemsDesc.item(i));
+                System.out.println("Description: ");
+                for (int j = 0; j < descriptions.getLength(); j++) {
+                    System.out.print(descriptions.item(j).getTextContent());
                 }
-                System.out.println("-----------------------");
+                System.out.println("--------------------");
             }
         } catch (Exception e) {
             throw e;
